087858c92e9fe383349e1bcc8df5862f9a274d90
Anastasios Tetradis <anastasios.tetradis@gmail.com>
2023-08-16 22:25:52 +0300

    Registration Number Page
    - Designed first page where client enters the registration number of their vehicle. (Client.java & ClientController.java)
    
    - Added FXML compatibility.
    
    - Added minimum and maximum window size.

--- Client/src/main/java/gr/uop/Client.java
+++ Client/src/main/java/gr/uop/Client.java
@@ -1,26 +1,40 @@
 package gr.uop;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * JavaFX App
  */
 public class Client extends Application {
 
+    private static Scene scene;
+
     @Override
-    public void start(Stage stage) {
-        var label = new Label("Hello, JavaFX Client");
-        var scene = new Scene(new StackPane(label), 640, 480);
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Client.class.getResource("registration-number.fxml"));
+        Parent mainPane = fxmlLoader.load();
+        scene = new Scene(mainPane, 1024, 768);
         stage.setScene(scene);
+        stage.setTitle("Client - Registration Input Page");
+
         stage.show();
+        
+        // Setting Max and Minimum Size
+        stage.setMinWidth(1024);
+        stage.setMinHeight(768);
+
+        stage.setMaxWidth(1920);
+        stage.setMaxHeight(1080);
     }
 
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
 
 }
---
+++ Client/src/main/java/gr/uop/ClientController.java
@@ -0,0 +1,5 @@
+package gr.uop;
+
+public class ClientController {
+
+}
