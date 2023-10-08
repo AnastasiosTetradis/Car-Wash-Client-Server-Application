4b2b9eeeb9e4ce04b4e8109aff420bc7ffd906bf
SILVERARGYROS <76134085+SILVERARGYROS@users.noreply.github.com>
2023-10-08 22:31:23 +0300

    Server Setup
    - Set up server project folder so that it can run fxml files.
    
    - Added resources from Client folder to Server folder.

--- Server/src/main/java/gr/uop/Server.java
+++ Server/src/main/java/gr/uop/Server.java
@@ -1,26 +1,45 @@
 package gr.uop;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableArray;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.URL;
+import java.util.ArrayList;
 
 /**
  * JavaFX App
  */
 public class Server extends Application {
 
+    private static Scene scene;
+
     @Override
-    public void start(Stage stage) {
-        var label = new Label("Hello, JavaFX Server");
-        var scene = new Scene(new StackPane(label), 640, 480);
+    public void start(Stage stage) throws IOException, ClassNotFoundException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Server.class.getResource("cashier.fxml"));
+        Parent mainPane = fxmlLoader.load();
+
+        Scene scene = new Scene(mainPane, 1024, 768);
         stage.setScene(scene);
+        stage.setTitle("Client - Registration Input Page");
+
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
 
 }
